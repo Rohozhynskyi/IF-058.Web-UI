df3ad279eee3d4968847424cdf23cf89989898f1
Sergepl1 <sergei_plahuta@mail.ru>
2015-12-15 23:13:44 +0200

    part of functionality of prepareCtrl  moved to service

--- src/app/conrollers/prepareToTestCtrl.js
+++ src/app/conrollers/prepareToTestCtrl.js
@@ -2,108 +2,29 @@ var testPlayerApp = angular.module('testPlayerApp', ['ui.router']);
 //список вопросов
 testPlayerApp.controller('prepareToTestCtrl', ['$scope', '$rootScope', 'userSrvc', '$stateParams', '$state', '$q', '$timeout', function ($scope, $rootScope, userSrvc, $stateParams, $state, $q, $timeout) {
 
-
-  var testData = {
-    counter: '',
-    startTime: '',
-    questionList: '',
-    timeForTest: '',
-    rate: [],
-    maxAvilable: 0
+  function getId () {
+    var id = $stateParams.id
+    return id
   }
-
-  var data = localStorage.userId
-  var url = 'result/getRecordsByStudent/'
   var id = $stateParams.id
-
-  function unique(resp) {
-    var repeatedTest_id = {
-      test_id: '',
-      result: ''
-    }
-    for (var i = 0; i < resp.data.length; i++) {
-      if (resp.data[i].test_id == id) {
-        repeatedTest_id.test_id = resp.data[i].test_id;
-        repeatedTest_id.result++;
-      }
-    }
-    return repeatedTest_id
-  }
-
+ 
   $scope.showInformModal = function (infMsg) {
     $scope.infMsg = infMsg;
     angular.element(document.querySelector('#informModal')).modal();
   };
-  $scope.getRecordsByStudent = function () {
-    userSrvc.getInfoForStudent(url, data).then(function (resp) {
-      var data = unique(resp).test_id
-      var result = unique(resp).result
-      var url = 'test/getRecords/'
-      return userSrvc.getInfoForStudent(url, data, result)
-    }).then(function (test) {
-      //$scope.timeForTest = test[0].data[0].time_for_test;
-      testData.timeForTest = test[0].data[0].time_for_test;
-      if (test[0].data[0].attempts < test[1]) {
-        alert('Немає предметів з доступними тестами для вашої групи')//убрати коли запрацює модалка
-        //$scope.showInformModal("Немає предметів з доступними тестами для вашої групи");
-      }
-
-      var data = $stateParams.id
-      var url = 'TestDetail/getTestDetailsByTest/'
-
-      return userSrvc.getInfoForStudent(url, data)
-    }).then(function (resp) {
-      if (!resp.data[0].id) {
-        alert('Немає деталей тесту')
-        return
-        //$scope.showInformModal("Немає деталей тесту");
-        //дописати щоб запрацювала модалка
-      } else {
-        for (var i = 0; i<resp.data.length; i++){
-          testData.maxAvilable += resp.data[i].tasks * resp.data[i].rate
-        }
-        var id = $stateParams.id
-        localStorage.testId = id;
-        var url = 'question/getQuestionIdsByLevelRand/'
-        var dataLevels = []
-        for (var i = 0; i<resp.data.length; i++){
-           dataLevels.push([id, resp.data[i].level, resp.data[i].tasks])
-          testData.rate.push({'level': resp.data[i].level, 'rate': resp.data[i].rate})
-
-           
-        }
-          function reqArr () {
-            var newArr = []
-            for (var k = 0; k<dataLevels.length; k++){
-              newArr.push(userSrvc.getInfoForStudent(url, dataLevels[k]))
-            }
-            return newArr
-          }
-         return $q.all(
-          reqArr()
-          )
-      }
-    }).then(function (resp) {
-      var questionList = []
-     for (var i = 0; i<resp.length; i++){
-        for (k in resp[i].data) {
-          questionList.push(resp[i].data[k].question_id)
-        }
-      testData.questionList = questionList;
-      $scope.questionsQuantity = questionList.length;
-     }
+  $scope.getRecordsByStudent = function(getId){
+    // console.log('check',userSrvc.checkStudent(id))
+    userSrvc.checkStudent(id).then(function(resp){
+      $scope.questionsQuantity = resp.questionList.length
+      $scope.testData = resp
+      console.log('$scope.testData', $scope.testData)
     })
-  }
-  var data = '';
-  var url = 'TestPlayer/getTimeStamp';
-  userSrvc.getInfoForStudent(url, data).then(function (resp) {
-    testData.startTime = resp.data.unix_timestamp
-  });
-  $scope.getRecordsByStudent()
+  } 
+  $scope.getRecordsByStudent(getId);
   $scope.startTest = function () {
     function saveData (){
       var url = 'testPlayer/saveData';
-      var data = testData;
+      var data = $scope.testData;
       userSrvc.postInfoForStudent(url, data)
     }
     saveData()
--- src/app/services/userSrvc.js
+++ src/app/services/userSrvc.js
@@ -1,5 +1,85 @@
-testPlayerApp.factory('userSrvc', ['$http', 'baseUrl', function ($http, baseUrl) {
+testPlayerApp.factory('userSrvc', ['$http', 'baseUrl', '$q', function ($http, baseUrl, $q) {
+  var testData = {
+    //counter: '',
+    //startTime: '',
+    questionList: '',
+    timeForTest: '',
+    rate: [],
+    maxAvilable: 0
+  }
+
+
+
+
   return {
+    checkStudent: function (id) {
+      var id = id
+      var questQty = ''
+      var data = localStorage.userId
+      //console.log('userId', data)
+      var url = 'result/getRecordsByStudent/'
+   return getInfoHelper(url, data).then(function (resp) {
+      var data = defineAttempts(resp).test_id
+      var result = defineAttempts(resp).result
+      var url = 'test/getRecords/'
+      return getInfoHelper(url, data, result)
+    }).then(function (test) {
+      //$scope.timeForTest = test[0].data[0].time_for_test;
+      testData.timeForTest = test[0].data[0].time_for_test;
+      if (test[0].data[0].attempts < test[1]) {
+        alert('Немає предметів з доступними тестами для вашої групи')//убрати коли запрацює модалка
+      }
+      var url = 'TestDetail/getTestDetailsByTest/'
+
+      return getInfoHelper(url, id)
+    }).then(function (resp) {
+      if (!resp.data[0].id) {
+        alert('Немає деталей тесту')
+        return
+      } else {
+        for (var i = 0; i<resp.data.length; i++){
+          testData.maxAvilable += resp.data[i].tasks * resp.data[i].rate
+        }
+        localStorage.testId = id;
+        var url = 'question/getQuestionIdsByLevelRand/'
+        var dataLevels = []
+        for (var i = 0; i<resp.data.length; i++){
+           dataLevels.push([id, resp.data[i].level, resp.data[i].tasks])
+          testData.rate.push({'level': resp.data[i].level, 'rate': resp.data[i].rate})
+
+           
+        }
+          function reqArr () {
+            var newArr = []
+            for (var k = 0; k<dataLevels.length; k++){
+              newArr.push(getInfoHelper(url, dataLevels[k]))
+            }
+            return newArr
+          }
+         return $q.all(
+          reqArr()
+          )
+      }
+    }).then(function (resp) {
+      var questionList = []
+     for (var i = 0; i<resp.length; i++){
+        for (k in resp[i].data) {
+          questionList.push(resp[i].data[k].question_id)
+        }
+      testData.questionList = questionList;
+      console.log('questionList.length', questionList.length)
+//return /*$scope.questionsQuantity =*/ questionList.length;
+     }
+     //questQty = questionList.length
+     console.log('questQty', questQty)
+     console.log('testData', testData)
+
+     return testData
+     //return /*$scope.questionsQuantity =*/ questionList.length;
+    })
+  },
+
+
     getInfoForStudent: function (url, data, result) {
       if (Array.isArray(data)){
         var sum = ''
@@ -28,6 +108,50 @@ testPlayerApp.factory('userSrvc', ['$http', 'baseUrl', function ($http, baseUrl)
 
     }
   }
+
+  function defineAttempts (resp, id) {
+    var repeatedTest_id = {
+      test_id: '',
+      result: ''
+    }
+    console.log('resp', resp)
+    for (var i = 0; i < resp.data.length; i++) {
+      console.log('resp.data[i].test_id', resp.data[i].test_id)
+      if (resp.data[i].test_id == id) {
+        repeatedTest_id.test_id = resp.data[i].test_id;
+        repeatedTest_id.result++;
+      }
+    }
+    console.log('repeatedTest_id', repeatedTest_id)
+    return repeatedTest_id
+  }
+
+  function getInfoHelper (url, data, result) {
+      if (Array.isArray(data)){
+        var sum = ''
+        for (i=0; i<data.length; i++){
+          sum = sum + data[i]+'/'
+        }
+        data = sum
+      }
+      if (url == 'test/getRecords/'){
+        return $http.get(baseUrl + url + data)
+        .then(function(response){
+            return [response, result];
+        }, rejected);
+      } else {
+        return $http.get(baseUrl + url + data)
+          .then(fulfilled, rejected);
+      }
+    }
+
+    function postInfoHelper (url, postData) {
+      return $http.post(baseUrl + url, postData)
+        .then(fulfilled, rejected);
+
+    }
+
+
   function fulfilled(response) {
     return response;
   };
